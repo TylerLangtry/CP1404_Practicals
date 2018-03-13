6e78a25bcc624d1d0eaa1b04a55c8f1d91fc8579
TylerLangtry <tyler.langtry@my.jcu.edu.au>
2018-03-13 10:35:46 +1000

    Add main and sub functions

--- prac_03/refactor_practice_2.py
+++ prac_03/refactor_practice_2.py
@@ -3,13 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status using functions
 """
 
-score = float(input("Enter score: "))
+def main():
 
-if score > 100 or score < 0:
-    print("Invalid Score")
-elif score == 100 or score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+    score = get_score()
+
+    if score > 100 or score < 0:
+        print("Invalid Score")
+    elif score == 100 or score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Pass")
+    else:
+        print("Bad")
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
