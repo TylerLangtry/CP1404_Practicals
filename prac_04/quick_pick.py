04f5a2adb10244de3f216e0eb55c8596de4ca81f
TylerLangtry <tylerlangtry@live.com>
2018-03-20 09:28:29 +1000

    Add fixed function names

--- prac_04/quick_pick.py
+++ prac_04/quick_pick.py
@@ -7,30 +7,30 @@ def main():
 
     quick_picks = int(input("How many Quick Picks? "))
 
-    lottery_generator(lottery, quick_picks)
+    generate_quickpick(lottery, quick_picks)
 
 
-def duplicate_check(number, line):
+def check_duplicates(number, line):
     if number in line:
         return True
     else:
         return False
 
 
-def number_generator():
+def generate_numbers():
     number = randint(1, 45)
     return number
 
 
-def lottery_generator(lottery, quick_picks):
+def generate_quickpick(lottery, quick_picks):
     for i in range(quick_picks):
         line = [0, 0, 0, 0, 0, 0]
         for j in range(6):
-            number = number_generator()
-            check = duplicate_check(number, line)
+            number = generate_numbers()
+            check = check_duplicates(number, line)
             while check == True:
-                number = number_generator()
-                check = duplicate_check(number, line)
+                number = generate_numbers()
+                check = check_duplicates(number, line)
             line[j] = number
         lottery.append(line)
     for row in lottery:
