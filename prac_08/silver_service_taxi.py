70f108237e56e24041e71fe0faa71139b2de0a52
TylerLangtry <tylerlangtry@live.com>
2018-05-08 00:10:02 +1000

    Add Taxi Simulator

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -3,7 +3,6 @@ from prac_08.Taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
-
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
@@ -11,9 +10,7 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = (Taxi.price_per_km*fanciness)
 
     def __str__(self):
-        return "{}, {}km on current fare, ${:.2f}/km plus flagfall of ${:.2f}".format(super().__str__(),
-                                                             self.current_fare_distance,
-                                                             self.price_per_km, self.flagfall)
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
 
     def get_fare(self):
         return super().get_fare()+self.flagfall
--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,13 +1,17 @@
 from prac_08.Taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
+
 def main():
-    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
-             SilverServiceTaxi("Hummer", 200, 4)]
+    taxi_1 = Taxi("Prius", 100)
+    taxi_2 = SilverServiceTaxi("Limo", 100, 2)
+    taxi_3 = SilverServiceTaxi("Hummer", 200, 4)
+    taxis = [taxi_1, taxi_2,taxi_3]
+    bill = 0
+    run_menu(taxis, bill)
 
-    taxis = run_menu(taxis)
 
-def run_menu(taxis):
+def run_menu(taxis, bill):
     """Display a menu and run the appropriate function based off user input"""
     menu = """Menu:
 Q - Quit
@@ -18,28 +22,40 @@ D - Drive"""
     while choice.upper() != "Q":
         if choice.upper() == "C":
             chosen_taxi = choose_taxi(taxis)
+            bill = calculate_bill(bill, 0)
         elif choice.upper() == "D":
-            distance = input("Drive how far?:")
-            taxis[chosen_taxi].name.drive(distance)
+            distance = int(input("Drive how far?:"))
+            taxis[chosen_taxi].drive(distance)
+            cost = taxis[chosen_taxi].get_fare()
+            print("Your {} trip cost you ${}".format(taxis[chosen_taxi].name, cost))
+            bill = calculate_bill(bill, cost)
         else:
             print("Invalid menu choice")
         print(menu)
         choice = input(">>> ")
-    print("b")
+    print("Total trip cost: ${:.2f}".format(bill))
+    print("Taxis are now:")
+    list_number = 0
+    for taxi in taxis:
+        print("{} - {}".format(list_number, taxi))
+        list_number += 1
     return taxis
 
 
 def choose_taxi(taxis):
     list_number = 0
+    print("Taxis Available:")
     for taxi in taxis:
         print("{} - {}".format(list_number, taxi))
         list_number += 1
-    choice = input("Choose taxi:")
-    print("Bill to date: {}".format(calculate_bill()))
+    choice = int(input("Choose taxi:"))
     return choice
 
 
-#def calculate_bill():
+def calculate_bill(bill, cost):
+    bill += cost
+    print("Bill to date: ${:.2f}".format(bill))
+    return bill
 
 
 main()
