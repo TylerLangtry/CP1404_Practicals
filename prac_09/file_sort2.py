e1e5a5debc4481a997fd524c5666d580da4d7281
TylerLangtry <tylerlangtry@live.com>
2018-05-14 22:16:08 +1000

    2

--- prac_09/file_sort2.py
+++ prac_09/file_sort2.py
@@ -7,17 +7,20 @@ print("Current directory is", os.getcwd())
 print(os.listdir('.'))
 
 extensions = []
+dir_names = []
+extension_directory = {}
 for filename in os.listdir('.'):
     (name, extension) = os.path.splitext(filename)
     print(extension)
 
     if extension not in extensions:
         extensions.append(extension)
-        dir_name = extension.strip(".")
-        os.mkdir(dir_name)
+        dir_name = input("What category would you like to sort {} files into?".format(extension.strip(".")))
+        extension_directory[extension] = dir_name
+        if dir_name not in dir_names:
+            dir_names.append(dir_name)
+            os.mkdir(dir_name)
         shutil.move(filename, dir_name)
     elif extension in extensions:
-        dir_name = extension.strip(".")
+        dir_name = extension_directory[extension]
         shutil.move(filename, dir_name)
-
-print(extensions)
